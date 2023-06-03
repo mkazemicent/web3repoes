c2d6a44f9a3edd1ba5620160c0eaf7856026b82b
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-06-03 14:38:50 -0400

    Update app.js
    app.js update with comments

--- AS1/my-portfoliov2/app.js
+++ AS1/my-portfoliov2/app.js
@@ -1,16 +1,18 @@
-// Mohammad Kazemi 301313841  COMP229 - Web Application Development Assignment 1
+// Mohammad Kazemi 301313841  COMP229 - Web Application Development Assignment 1 app.js file
+// library imports
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-
+// body parser middleware setup 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// view engine setup
 app.set('view engine', 'ejs');
-
+// static assets setup
 app.use(express.static('public'));
-
+// routes setup
 app.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
@@ -26,15 +28,16 @@ app.get('/services', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact', { title: 'Contact' });
 });
-
+// POST route from contact form
 app.post('/contact', (req, res) => {
     let data = `${req.body.name} (${req.body.email}): ${req.body.message}\n`;
     fs.appendFile(path.join(__dirname, 'messages.txt'), data, (err) => {
         if (err) throw err;
     });
+    // redirect to home page
     res.redirect('/');
 });
-
+//the projects to be displayed and routed to
 app.get('/projects', (req, res) => {
     const projects = [
         {
@@ -55,7 +58,7 @@ app.get('/projects', (req, res) => {
     ];
     res.render('projects', { title: 'Projects', projects });
 });
-
+// server setup get port from environment variable or default to 3000-- prepare for cloud deployment azure!
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
