a6c6eaf67b7d8de8abb7516d6d031f191d89b5d2
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-06-03 03:08:09 -0400

    deleted the old project

--- my-portfolio/routes/projects.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res) {
-  res.render('projects', { title: 'Projects' });
-});
-
-module.exports = router;
--- my-portfolio/routes/services.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res) {
-    res.render('services', { title: 'Services' });
-});
-
-module.exports = router;
