6c6ab19592761b18b9b75d771dcd831ec62141b4
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-06-03 01:59:32 -0400

    Content added!
    -content added
    -logo added
    -missing profile photo  and resume
    -missing comments
    -i hate this assignemnt

--- AS1/my-portfoliov2/app.js
+++ AS1/my-portfoliov2/app.js
@@ -1,13 +1,60 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.get('/services', (req, res) => {
+    res.render('services', { title: 'Services' });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact' });
+});
+
+app.post('/contact', (req, res) => {
+    let data = `${req.body.name} (${req.body.email}): ${req.body.message}\n`;
+    fs.appendFile(path.join(__dirname, 'messages.txt'), data, (err) => {
+        if (err) throw err;
+    });
+    res.redirect('/');
+});
+
+app.get('/projects', (req, res) => {
+    const projects = [
+        {
+            title: 'kloudDNA',
+            description: 'A platform that uses AI to tag and create a "DNA" for books, movies, and music. It will analyze users\' past preferences and current mood to suggest personalized content recommendations.',
+            link: '/'
+        },
+        {
+            title: 'CDN Project',
+            description: 'A custom Content Delivery Network (CDN) solution aimed at enhancing web content delivery speed and reliability.',
+            link: '/'
+        },
+        {
+            title: 'Portfolio Website',
+            description: 'This portfolio website is a showcase of my work and skills in web development. It is a testament to my technical abilities and my knack for clean, intuitive design.',
+            link: '/'
+        }
+    ];
+    res.render('projects', { title: 'Projects', projects });
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
