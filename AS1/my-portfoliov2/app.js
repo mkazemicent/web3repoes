839ffd1db2f82026caf2b29b24e0cc3d78f91517
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-06-03 14:10:07 -0400

    Preparing for Azure
    -Changing environment variable for port instead of 3000

--- AS1/my-portfoliov2/app.js
+++ AS1/my-portfoliov2/app.js
@@ -55,6 +55,8 @@ app.get('/projects', (req, res) => {
     res.render('projects', { title: 'Projects', projects });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
+
